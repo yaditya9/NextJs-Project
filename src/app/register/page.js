3ae918639de5a2cfa2d1af6ed5c1418c772829a2
Aditya <yaditya@deloitte.com>
2023-12-07 21:28:45 +0530

    Added grid for responsie layout

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Box } from "@mui/material";
 
 import {
   Card,
@@ -9,12 +8,14 @@ import {
   FormControlLabel,
   Checkbox,
   Button,
+  Box,
+  Grid,
 } from "@mui/material";
 
 const RegisterPage = () => {
   return (
     <Box
-      style={{
+      sx={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
@@ -25,143 +26,103 @@ const RegisterPage = () => {
         elevation={8}
         sx={{
           maxWidth: 1000,
-          minWidth: 600,
+          minWidth: 300, // Smaller minWidth for smaller screens
           borderRadius: "24px",
-          height: "95vh",
-          /* backgroundColor: "lavender", */
+          height: "auto", // Changed to 'auto' for better responsiveness
           backgroundImage: "linear-gradient(to right, #ffafbd, #ffc3a0)",
         }}
       >
         <CardContent>
-          <Typography
-            variant="h4"
-            gutterBottom
-            sx={{ fontFamily: "Roboto, sans-serif", fontWeight: 500 }}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "60px",
-            }}
-          >
-            Get started!
-          </Typography>
-          <Box
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              width: "100%",
-              marginTop: "30px",
-            }}
-          >
-            <TextField
-              label="Email"
-              type="email"
-              variant="outlined"
-              margin="normal"
-              sx={{
-                "& .MuiInputBase-input": {
-                  height: "15px", // Adjust this value as needed
-                  /* padding: "5px", // Adjust padding to control height */
-                  fontSize: "14px", // Optional: Adjust font size if needed
-                },
-                width: "60%",
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "12px", // Rounds the corners of the outline
-                  backgroundColor: "white",
-                },
-              }}
-            />
-          </Box>
-          <Box
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              width: "100%",
-              marginTop: "20px",
-            }}
-          >
-            <TextField
-              label="Name"
-              type="text"
-              variant="outlined"
-              sx={{
-                "& .MuiInputBase-input": {
-                  height: "15px", // Adjust this value as needed
-                  /* padding: "5px", // Adjust padding to control height */
-                  fontSize: "14px", // Optional: Adjust font size if needed
-                },
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "12px", // Rounds the corners of the outline
-                  backgroundColor: "white",
-                },
-                width: "60%",
-              }}
-              margin="normal"
-            />
-          </Box>
-
-          <FormControlLabel
-            control={<Checkbox />}
-            label={
-              <>
-                Agree if you have accepted to
+          <Grid container spacing={2} justifyContent="center">
+            {" "}
+            {/* Grid container */}
+            <Grid item xs={12}>
+              {" "}
+              {/* Full width on all screens */}
+              <Typography
+                variant="h4"
+                gutterBottom
+                sx={{
+                  fontFamily: "Roboto, sans-serif",
+                  fontWeight: 500,
+                  textAlign: "center",
+                }}
+              >
+                Get started!
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={8} md={8} lg={8} xl={8}>
+              {" "}
+              {/* Adjusts size based on screen */}
+              <TextField
+                label="Email"
+                type="email"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={8} md={8} lg={8} xl={8}>
+              <TextField
+                label="Name"
+                type="text"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={8} md={8} lg={8} xl={8}>
+              <FormControlLabel
+                control={<Checkbox />}
+                label={
+                  <>
+                    Agree if you have accepted to
+                    <a
+                      href="https://generator.lorem-ipsum.info/terms-and-conditions"
+                      style={{ textDecoration: "none", marginLeft: "5px" }}
+                    >
+                      terms and conditions
+                    </a>
+                  </>
+                }
+                sx={{ justifyContent: "center" }}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={8}
+              lg={8}
+              xl={8}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ maxWidth: "60%", minWidth: "60%", margin: "0 auto" }}
+              >
+                Submit
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography
+                variant="body2"
+                component="div"
+                gutterBottom
+                sx={{ fontSize: "12px", textAlign: "center" }}
+              >
+                Already registered?{"   "}
                 <a
-                  href="https://generator.lorem-ipsum.info/terms-and-conditions"
+                  href="https://www.google.com"
                   style={{ textDecoration: "none", marginLeft: "5px" }}
                 >
-                  terms and conditions
+                  Sign In
                 </a>
-              </>
-            }
-            sx={{
-              "& .MuiFormControlLabel-label": {
-                // Targeting the label specifically
-                fontSize: "13px", // Adjust the font size as needed
-              },
-              marginLeft: "105px",
-              marginTop: "10px",
-            }}
-          />
-          <Box
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              width: "100%",
-              marginTop: "30px",
-            }}
-          >
-            <Button
-              variant="contained"
-              color="primary"
-              fullWidth
-              style={{ marginTop: "30px" }}
-              sx={{ width: "60%" }}
-            >
-              Submit
-            </Button>
-          </Box>
-          <Typography
-            variant="body2"
-            component="div"
-            gutterBottom
-            sx={{
-              fontSize: "12px",
-            }}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "10px",
-            }}
-          >
-            Already registered?{"   "}
-            <a
-              href="https://www.google.com"
-              style={{ textDecoration: "none", marginLeft: "5px" }}
-            >
-              Sign In
-            </a>
-            .
-          </Typography>
+                .
+              </Typography>
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
     </Box>
